b90d17a33fe779d6d59bb746d8acaff5434743c0
theRex <olosun.m1904582@st.futminna.edu.ng>
2026-01-18 03:53:46 +0100

    A simple python quiz game

---
+++ quiz_brain.py
@@ -0,0 +1,32 @@
+class QuizBrain:
+    def __init__(self, question_list):
+        self.question_number = 0
+        self.question_list = question_list
+        self.score = 0
+
+
+    def next_question(self):
+        current_question = self.question_list[self.question_number]
+        question_answer = current_question.answer
+        self.question_number += 1
+        user_answer = input(f"Q.{self.question_number}: {current_question.text} (True/False) ")
+        self.check_answer(user_answer, question_answer)
+
+
+    def still_has_question(self):
+        total_questions = len(self.question_list)
+        if self.question_number < total_questions:
+            return True
+        else:
+            return False
+
+
+    def check_answer(self, u_answer, correct_answer):
+        if u_answer.lower() == correct_answer.lower():
+            print("You got it right")
+            self.score += 1
+        else:
+            print("You got it wrong!")
+        print(f"The correct answer was: {correct_answer}")
+        print(f"Your current score is: {self.score}/{self.question_number}.")
+        print("\n")
